eaf95aaf7c0e779b9f8465781167a7a190910b81
LalithyaMaddumage <sandeepanilalithya155@gmail.com>
2023-06-02 01:57:32 +0530

    Email verification and individual register details forms done

--- frontend/src/pages/regIndividual.js
+++ frontend/src/pages/regIndividual.js
@@ -52,7 +52,7 @@ const RegIndividual = () => {
             {currentStep === 1 && (
               <div>
                 {/* Form fields for step 1 */}
-                <div className='h-[45px] mt-[28px] ml-[1145px]  font-Montserrat font-semibold leadding-[44px] font-[25px] text-3xl/[25px] '>Register as Individual</div>
+                <div className='h-[45px] mt-[28px] ml-[1145px]  font-Montserrat font-semibold leadding-[44px] font-[25px] text-3xl/[25px] ' >Register as Individual</div>
                 {/* <button
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                   onClick={handleNextStep}
@@ -70,7 +70,7 @@ const RegIndividual = () => {
             />
           </div>
           <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px]  mt-[120px] ml-[881px]  ">
-            <button className="absolute w-[588px] h-[84px]  border-[1px] border-solid border-[#01B3BB] rounded-[10px] hover: border-[1px] border-solid border-[#01B3BB] rounded-[10px] bg-[#01B3BB] text-white font-semibold font-Montserrat  leading-[22px]">
+            <button className="absolute w-[588px] h-[84px]  border-[1px] border-solid border-[#01B3BB] rounded-[10px] hover: border-[1px] border-solid border-[#01B3BB] rounded-[10px] bg-[#01B3BB] text-white font-semibold font-Montserrat  leading-[22px]"  onClick={handleNextStep}>
               CONTINUE
             </button>
           </div>
@@ -90,37 +90,157 @@ const RegIndividual = () => {
             {currentStep === 2 && (
               <div>
                 {/* Form fields for step 2 */}
-                <input type="text" className="w-full mb-4" placeholder="Field 2" />
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  onClick={handleNextStep}
-                >
-                  Next
-                </button>
-                <button
-                  className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
-                  onClick={handlePreviousStep}
-                >
-                  Previous
-                </button>
+                <div className='h-[45px] mt-[28px] ml-[1200px]  font-Montserrat font-semibold leadding-[44px] font-[25px] text-3xl/[25px] '>Verify Your Email </div>
+                
+                <div className=' h-[45px] ml-[1055px]  mr-[260px]  mt-[19px] leading-[26px] text-sm font-[Open Sans] font-normal text-right  text-[#707070]'>
+                we have sent you a code into your email ****@gmail.com please type it below and confirm
+            </div>
+
+                <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px]  mt-[275px] ml-[881px]  ">
+                
+            <input
+              className="absolute w-[84px] h-[84px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px]"
+           
+            />
+            
+          </div>
+          <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px]   ml-[1007px]  ">
+                
+                <input
+                  className="absolute w-[84px] h-[84px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px]"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px]   ml-[1133px]  ">
+                
+                <input
+                  className="absolute w-[84px] h-[84px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px]"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px]   ml-[1259px]  ">
+                
+                <input
+                  className="absolute w-[84px] h-[84px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px]"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px]   ml-[1385px]  ">
+                
+                <input
+                  className="absolute w-[84px] h-[84px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px]"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px]  mt-[120px] ml-[881px]  ">
+            <button className="absolute w-[588px] h-[84px]  border-[1px] border-solid border-[#01B3BB] rounded-[10px] hover: border-[1px] border-solid border-[#01B3BB] rounded-[10px] bg-[#01B3BB] text-white font-semibold font-Montserrat  leading-[22px]"  onClick={handleNextStep}>
+              CONTINUE
+            </button>
+          </div>
+          <div className="absolute ml-[1249px]  mt-[50px]  mr-[64px] h-[45px] ">
+            <div className="text-black font-semibold font-Montserrat text-2xl/[20px] leading-[44px]">
+              Don't recieve code ?
+            </div>
+          </div>
+          <div className="absolute  mt-[90px] ml-[1317px] mr-[64px] h-[45px] ">
+            <div className="text-[#01B3BB] font-semibold font-Montserrat text-2xl/[24px] leading-[44px] ">
+             Resend Code
+            </div>
+          </div>
+                
+                
+            
               </div>
             )}
             {currentStep === 3 && (
               <div>
                 {/* Form fields for step 3 */}
-                <input type="text" className="w-full mb-4" placeholder="Field 3" />
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  onClick={handleNextStep}
-                >
-                  Next
-                </button>
-                <button
-                  className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
-                  onClick={handlePreviousStep}
-                >
-                  Previous
-                </button>
+                <div className='h-[45px] mt-[28px] ml-[1130px]  font-Montserrat font-semibold leadding-[44px] font-[25px] text-3xl/[25px] '>Register as Individual </div>
+                <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px] mt-[35px]  ml-[881px]  ">
+                
+                <input
+                  className="absolute w-[284px] h-[50px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px] placeholder:text-[rgba(30, 30, 30, 0.7)] font-normal font-inter text-[12px] leading-[15px]"
+                  placeholder="Enter Your First Name"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px]   ml-[1185px]  ">
+                
+                <input
+                  className="absolute w-[284px] h-[50px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px] placeholder:text-[rgba(30, 30, 30, 0.7)] font-normal font-inter text-[12px] leading-[15px]"
+                  placeholder="Enter Your Last name"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px] mt-[75px]   ml-[881px]  ">
+                
+                <input
+                  className="absolute w-[588px] h-[50px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px] placeholder:text-[rgba(30, 30, 30, 0.7)] font-normal font-inter text-[12px] leading-[15px]"
+                  placeholder="Your Phone Number"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px] mt-[75px]   ml-[881px]  ">
+                
+                <input
+                  className="absolute w-[588px] h-[50px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px] placeholder:text-[rgba(30, 30, 30, 0.7)] font-normal font-inter text-[12px] leading-[15px]"
+                  placeholder="Address Line 1"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px] mt-[75px]   ml-[881px]  ">
+                
+                <input
+                  className="absolute w-[588px] h-[50px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px] placeholder:text-[rgba(30, 30, 30, 0.7)] font-normal font-inter text-[12px] leading-[15px]"
+                  placeholder="Address Line 1"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px] mt-[75px]  ml-[881px]  ">
+                
+                <input
+                  className="absolute w-[284px] h-[50px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px] placeholder:text-[rgba(30, 30, 30, 0.7)] font-normal font-inter text-[12px] leading-[15px]"
+                  placeholder="State"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px]   ml-[1185px]  ">
+                
+                <input
+                  className="absolute w-[284px] h-[50px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px] placeholder:text-[rgba(30, 30, 30, 0.7)] font-normal font-inter text-[12px] leading-[15px]"
+                  placeholder="Postal Code"
+               
+                />
+                
+              </div>
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px] mt-[75px]   ml-[881px]  ">
+                
+                <input
+                  className="absolute w-[588px] h-[50px] border-[1px] border-solid border-[#01B3BB] rounded-[10px] pl-[20px]  leading-[15px] placeholder:text-[rgba(30, 30, 30, 0.7)] font-normal font-inter text-[12px] leading-[15px]"
+                  placeholder="Country"
+               
+                />
+                
+              </div>
+
+              <div className="box-border flex flex-row items-center p-[14px 75px 14px 20px] gap-[10px]  mt-[95px] ml-[881px]  ">
+            <button className="absolute w-[588px] h-[84px]  border-[1px] border-solid border-[#01B3BB] rounded-[10px] hover: border-[1px] border-solid border-[#01B3BB] rounded-[10px] bg-[#01B3BB] text-white font-semibold font-Montserrat  leading-[22px]"  onClick={handleNextStep}>
+              CONTINUE
+            </button>
+            </div>
+               
+               
+               
               </div>
             )}
             {currentStep === 4 && (
